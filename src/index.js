410f9b2d53801f6473a67631390b88971774cbb1
Akshay Gupta <akshaygrkesh@gmail.com>
2021-05-04 22:08:48 +0530

    Add email notification

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
+'use strict';
+
 require('isomorphic-fetch');
+const nodemailer = require('nodemailer');
+const fs = require('fs');
 
 const cowinHost = "https://cdn-api.co-vin.in";
 
-async function checkForOpenSlots(districtId) {
+const config = JSON.parse(fs.readFileSync('src/config.json'));
+
+const emailer = nodemailer.createTransport({
+  service: config.email.service,
+  auth: {
+    user: config.email.user,
+    pass: config.email.password
+  }
+});
+
+async function main() {
+  const centers = await getCenters(config.districtId);
+  if (centers && centers.length > 0) {
+    sendNotificationEmail(JSON.stringify(centers));
+  }
+}
+
+async function getCenters(districtId) {
   const tomorrow = getTomorrow();
   const response = await fetch(`${cowinHost}/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${districtId}&date=${tomorrow}`);
   if (response.status !== 200) {
@@ -10,6 +31,23 @@ async function checkForOpenSlots(districtId) {
   }
   const data = await response.json();
   return getAvailableCenters(data.centers);
+}
+
+function sendNotificationEmail(emailContent) {
+  const mailOptions = {
+    from: config.email.user,
+    to: config.email.recipients,
+    subject: 'Covid Vaccination Center Available',
+    text: emailContent
+  };
+        
+  emailer.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.error(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
 }
   
 function getTomorrow() {
@@ -28,3 +66,5 @@ function getAvailableCenters(centers) {
   })
   .filter(center => center.sessions.length > 0);
 }
+
+main();
